bfdf9b184ff94ab2541c24944952d373de9c92e9
qbnyp <moneycloudy@naver.com>
2024-10-02 10:35:13 +0900

    2week 이미지 불러오기

--- umcweb/src/미션/2주차/2week.js
+++ umcweb/src/미션/2주차/2week.js
@@ -6,11 +6,14 @@ import {MOVIES} from './mocks/movies';
 
 export default function Index() {
 
+    const baseURL = "https://image.tmdb.org/t/p/w500"
+
     return (
         <>
         <main>
             //MOVIES의 배열중 RESULT가 첫번째인 값의 타이틀을 호출함
             {MOVIES.results[1].title}
+            <img src={baseURL + MOVIES.results[1].poster_path} alt="" />
         </main>
         </>
     )
